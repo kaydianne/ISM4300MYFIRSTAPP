9417378d6b3d32c4eefef6b704cf317c9c551f4e
Kay <Kaydi@DESKTOP-U7ER47O>
2019-08-29 16:29:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single line comments
+
+            Console.WriteLine("This is my first Console App");
+            Console.WriteLine("I am learning how to publish to GitHub");
+        }
+    }
+}
